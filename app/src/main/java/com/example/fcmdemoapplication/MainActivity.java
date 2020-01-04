59bec05f61a68e7fb4680ceab85818b7d80b3ed1
joehang0113 <joechang0113@gmail.com>
2020-01-04 22:22:43 +0800

    first commit

---
+++ app/src/main/java/com/example/fcmdemoapplication/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.fcmdemoapplication;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+
+                        if (!task.isSuccessful()) {
+                            return;
+                        }
+                        if( task.getResult() == null)
+                            return;
+                        // Get new Instance ID token
+                        String token = task.getResult().getToken();
+                        // Log and toast
+                        Log.i("MainActivity","token "+token);
+                    }
+                });
+    }
+}
---
+++ app/src/main/java/com/example/fcmdemoapplication/MyFirebaseService.java
@@ -0,0 +1,26 @@
+package com.example.fcmdemoapplication;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        if (remoteMessage.getNotification() != null) {
+            Log.i("MyFirebaseService","title "+remoteMessage.getNotification().getTitle());
+            Log.i("MyFirebaseService","body "+remoteMessage.getNotification().getBody());
+        }
+    }
+
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+
+        Log.i("MyFirebaseService","token "+s);
+    }
+}
